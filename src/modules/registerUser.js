e6d8c6075d29f9ad9063319f1a738297aafdee58
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-04-27 09:49:26 +0100

    added the latest changes

--- src/modules/registerUser.js
+++ src/modules/registerUser.js
@@ -1,6 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
-
+const db = new sqlite3.Database('db.sqlite');
 const bcrypt = require('bcrypt');
 const validator = require('validator');
 
@@ -31,32 +30,4 @@ function registerUser(name, email, password, callback) {
   });
 }
 
-function loginUser(email, password, callback) {
-  const stmt = db.prepare('SELECT * FROM users WHERE email = ?');
-  stmt.get(email, function(err, row) {
-    if (err) {
-      return callback(err);
-    }
-
-    if (!row) {
-      return callback('User not found');
-    }
-
-    bcrypt.compare(password, row.password, function(err, result) {
-      if (err) {
-        return callback(err);
-      }
-
-      if (!result) {
-        return callback('Invalid password');
-      }
-
-      callback(null, row.id);
-    });
-  });
-}
-
-module.exports = {
-  registerUser,
-  loginUser
-};
+module.exports = registerUser;
