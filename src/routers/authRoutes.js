a05f2dc9ab59f084f2262bc09765a2a198d7798b
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-04-27 10:05:43 +0100

    added the latest views

--- src/routers/authRoutes.js
+++ src/routers/authRoutes.js
@@ -1,29 +1,26 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser, loginUser } = require('../models/userModel');
-
-// Register a new user
-router.post('/register', async (req, res) => {
-  const { name, email, password } = req.body;
-
-  try {
-    await registerUser(name, email, password);
-    res.status(201).send({ message: 'User created successfully' });
-  } catch (error) {
-    res.status(400).send({ error: error.message });
-  }
-});
-
-// Login a user
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const userId = await loginUser(email, password);
-    res.status(200).send({ message: 'User logged in successfully', userId });
-  } catch (error) {
-    res.status(401).send({ error: error.message });
-  }
-});
-
-module.exports = router;
+const loginUser = require('../models/loginUser');
+const registerUser = require('../models/registerUser');
+app.post('/api/register', function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    registerUser(email, password, (err, result) => {
+      if (err) {
+        res.status(500).json({ error: err });
+      } else {
+        res.status(200).json(result);
+      }
+    });
+  });
+  
+  app.post('/api/login', function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    loginUser(email, password, (err, result) => {
+      if (err) {
+        res.status(500).json({ error: err });
+      } else {
+        res.status(200).json(result);
+      }
+    });
+  });
+  
